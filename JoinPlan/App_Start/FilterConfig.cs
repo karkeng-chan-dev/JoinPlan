70ce0fefbc06ea3dd6299e177a854e2f4bc5c1cd
Chan Kar Keng <karkeng.chan@gmail.com>
2020-05-05 00:34:57 +0800

    Add project files.

---
+++ JoinPlan/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JoinPlan
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
